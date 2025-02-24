82606d45a141de610aa7b421716f1fbfd7ae986d
Joshua <steedjms@gmail.com>
2025-02-23 18:50:23 -0700

    adds react variables and placeholder for user_profile_image

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,11 +1,17 @@
 import React from "react";
 
-export function Login() {
+export function Login(props) {
+    const [imageUrl, setImageUrl] = React.useState(`data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=`);
+
+    React.useEffect(() => {
+        setImageUrl('https://random.dog/201915e6-89e5-4811-8648-7c433d771af5.jpg');
+    })
+
     return (
         <main>
             <h1>Welcome to Fantasy General Conference</h1>
             <div>
-                <img className="profile" src="https://random.dog/201915e6-89e5-4811-8648-7c433d771af5.jpg" alt="Placeholder_Dog_User_Image" width="10%" height="auto" />
+                <img className="profile" src={imageUrl} alt="User_Profile_Image" width="10%" height="auto" />
             </div>
             <form id="user_info" method="get" action="guess.html">
             <div className="input_container">
