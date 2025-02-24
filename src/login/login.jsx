f570b9b90d7360512d8982b86c5ff7e02e52d4b0
Joshua <steedjms@gmail.com>
2025-02-23 19:26:02 -0700

    adds variables for input boxes and prepares the userName variable

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,6 +2,9 @@ import React from "react";
 
 export function Login(props) {
     const [imageUrl, setImageUrl] = React.useState(`data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=`);
+    const [userName, setUserName] = React.useState(props.userName);
+    // const [userEmail, setUserEmail] = React.userState(props.userEmail);
+    // const [password, setPassword] = React.userState('');
 
     React.useEffect(() => {
         setImageUrl('https://random.dog/201915e6-89e5-4811-8648-7c433d771af5.jpg');
@@ -16,7 +19,7 @@ export function Login(props) {
             <form id="user_info" method="get" action="guess.html">
             <div className="input_container">
                <span>👨‍💻</span> 
-               <input id="user_name" type="user" placeholder="username" />
+               <input id="user_name" type="user" value={userName} onChange={(e) => setUserName(e.target.value)} placeholder="username" />
             </div>
             <div className="input_container">
                 <span>📧</span>
