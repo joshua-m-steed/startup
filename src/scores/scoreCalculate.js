97789d9ff5843a55b4f154c78645698ada2d2aff
Joshua <steedjms@gmail.com>
2025-02-26 17:35:50 -0700

    feat(score calc) set up psuedo code for the scorecalulate function

---
+++ src/scores/scoreCalculate.js
@@ -0,0 +1,25 @@
+export class ScoreCalculator {
+    constructor(guess, answer)
+    {
+        this.guess = guess;
+        this.answer = answer;
+
+    }
+
+    #clean() 
+    {
+        // Clean the Answers
+        // - Lowercase
+        // - No Whitespace 
+    }
+
+    score() 
+    {
+        // Compares Guess with Answer
+    }
+
+    createTable() 
+    {
+        // Sets up the table row / Array
+    }
+}
