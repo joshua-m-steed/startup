6a4cd6878723fdec2d707cbac12ce3c879c7e603
Joshua <steedjms@gmail.com>
2025-03-09 13:02:22 -0600

    initializes more structure to the index.js code including parse, hosting static files, vite config,  and error handling

--- service/index.js
+++ service/index.js
@@ -1,4 +1,4 @@
-const cookiePareser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 const express = require('express');
 const uuid = require('uuid');
 const bcrypt = require('bcryptjs');
@@ -6,3 +6,26 @@ const bcrypt = require('bcryptjs');
 const app = express();
 
 const authCookieName = 'token';
+
+const port = process.argv > 2 ? process.argv[2] : 4000;
+
+// Declaring parsing and hosting static content
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(express.static('public'));
+
+// "Err", professional error catchers
+app.use(function ( err, req, res, next) {
+    res.status(500).send({ type: err.name, message: err.message });
+});
+
+// Router
+var apiRouter = express.Router();
+app.use('/api', apiRouter);
+
+// Ears are open O.O
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
