f1d2e21ebd818627924c86745579d5df7fbaea7e
Joshua <steedjms@gmail.com>
2025-04-07 18:11:53 -0600

    updates to hold messages and checks for messages before inputting

--- src/notes/notes.jsx
+++ src/notes/notes.jsx
@@ -12,17 +12,47 @@ export function Notes({ webSocket }) {
     );
 }
 
-    function Name({ updateName }) {
-        return (
-            <main>
-                <div className="name">
-                    <fieldset id='name-controls'>
-                        <legend>Choose Name</legend>
-                        <input onChange={(e) => updateName(e.target.value)} id='my-name' type='text' />
-                    </fieldset>
-                </div>
-            </main>
-        );
+// Holds box and variables for Name
+function Name({ updateName }) {
+    return (
+        <main>
+            <div className="name">
+                <fieldset id='name-controls'>
+                    <legend>Choose Name</legend>
+                    <input onChange={(e) => updateName(e.target.value)} id='my-name' type='text' />
+                </fieldset>
+            </div>
+        </main>
+    );
+}
+
+// Holds box for message
+function Message({ name, webSocket}) {
+    const [message, setMessage] = React.useState('');
+
+    // Sends message upon 'ENTER'
+    function doneMessage(e) {
+        if(e.key === 'Enter') {
+            sendMsg();
+        }
+    }
+
+    // Sends message to ws and resets variable
+    function sendMsg() {
+        webSocket.sendMessage(name, message);
+        setMessage('');
+    }
+
+    const disabled = name === '' || !webSocket.connected;
+    return (
+        <main>
+            <fieldset id='chat-controls'>
+                <legend>Message</legend>
+                <input disabled={disabled} onKeyDown={(e) => doneMessage(e)} value={message} />
+                <button disabled={disabled || !message} onClick={sendMsg}>Send</button>
+            </fieldset>
+        </main>
+    );
 }
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
