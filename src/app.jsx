c8a54a4cdde4dcb87e1e3e451a34384fb74497e0
Joshua <steedjms@gmail.com>
2025-09-27 15:21:25 -0600

    doc() forgot patch number in previous commit

--- src/app.jsx
+++ src/app.jsx
@@ -82,7 +82,7 @@ export default function App() {
                             </a>
                         </span>
                     </div> 
-                    <div className='version'>v.1.1.1</div>
+                    <div className='version'>v.1.1.2</div>
                         
                     
                 </footer>
