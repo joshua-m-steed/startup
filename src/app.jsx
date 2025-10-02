9f7b6f7552b1ac48bc0afe64025511db9cf5989c
Joshua <steedjms@gmail.com>
2025-10-01 22:34:10 -0600

    fix(final mobile patch) adjusting the flex box of Login CSS

--- src/app.jsx
+++ src/app.jsx
@@ -82,7 +82,7 @@ export default function App() {
                             </a>
                         </span>
                     </div> 
-                    <div className='version'>v.1.1.3</div>
+                    <div className='version'>v.1.1.4</div>
                         
                     
                 </footer>
